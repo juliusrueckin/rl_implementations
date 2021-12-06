a4aef89d3b435ec5f0a11347c21f3f3f13ff5255
Julius Rückin <julius.rueckin@gmail.com>
2021-12-06 18:01:50 +0100

    implement prioritized replay buffer

--- networks/q_network_wrappers.py
+++ networks/q_network_wrappers.py
@@ -3,7 +3,7 @@ import torch
 import constants as const
 from utils import utils
 from networks.q_networks import DQN
-from replay_buffers import ReplayBuffer
+from replay_buffers import ReplayBuffer, PrioritizedExperienceReplay
 from torch import nn
 
 
@@ -58,14 +58,16 @@ class DeepQLearningWrapper:
 class DoubleDeepQLearningWrapper:
     def __init__(self, screen_width: int, screen_height: int, num_actions: int):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.replay_buffer = ReplayBuffer(const.REPLAY_BUFFER_LEN)
+        self.replay_buffer = PrioritizedExperienceReplay(
+            const.REPLAY_BUFFER_LEN, const.BATCH_SIZE, const.ALPHA, const.BETA0, const.REPLAY_DELAY
+        )
 
         self.policy_net = DQN(screen_width, screen_height, num_actions).to(self.device)
         self.target_net = DQN(screen_width, screen_height, num_actions).to(self.device)
         self.target_net.load_state_dict(self.policy_net.state_dict())
         self.target_net.eval()
 
-        self.loss_fn = nn.SmoothL1Loss()
+        self.loss_fn = nn.SmoothL1Loss(reduction="none")
         self.optimizer = torch.optim.RMSprop(self.policy_net.parameters(), lr=const.LEARNING_RATE)
 
     def update_target_network(self):
@@ -75,7 +77,8 @@ class DoubleDeepQLearningWrapper:
         if len(self.replay_buffer) < const.BATCH_SIZE:
             return
 
-        transitions = self.replay_buffer.sample(const.BATCH_SIZE)
+        transitions, indices, weights = self.replay_buffer.sample()
+        weights = torch.FloatTensor(weights).to(self.device)
         batch = utils.Transition(*zip(*transitions))
         non_final_mask = torch.tensor(
             tuple(map(lambda s: s is not None, batch.next_state)), device=self.device, dtype=torch.bool
@@ -86,6 +89,13 @@ class DoubleDeepQLearningWrapper:
             torch.cat(batch.state),
             torch.cat(batch.action),
             torch.cat(batch.reward),
+        )
+
+        state_batch, action_batch, reward_batch, non_final_mask = (
+            state_batch.to(self.device),
+            action_batch.to(self.device),
+            reward_batch.to(self.device),
+            non_final_mask.to(self.device),
         )
 
         state_action_values = self.policy_net(state_batch).gather(1, action_batch)
@@ -100,11 +110,16 @@ class DoubleDeepQLearningWrapper:
             )
             expected_state_action_values = reward_batch + const.GAMMA * next_state_action_values
 
-        loss = self.loss_fn(state_action_values, expected_state_action_values.unsqueeze(1))
+        losses = self.loss_fn(state_action_values, expected_state_action_values.unsqueeze(1))
+        weighted_losses = weights * losses
+        weighted_loss = weighted_losses.mean()
         self.optimizer.zero_grad()
-        loss.backward()
+        weighted_loss.backward()
 
         for param in self.policy_net.parameters():
             param.grad.data.clamp_(-const.CLIP_GRAD, const.CLIP_GRAD)
 
         self.optimizer.step()
+
+        self.replay_buffer.step()
+        self.replay_buffer.update(indices, (losses + 1e-8).view(-1).data.cpu().numpy())
--- replay_buffers.py
+++ replay_buffers.py
@@ -1,5 +1,9 @@
 import random
 from collections import deque
+from typing import List, Tuple
+
+import numpy as np
+
 from utils import utils
 
 
@@ -12,6 +16,61 @@ class ReplayBuffer:
 
     def sample(self, batch_size: int = 32):
         return random.sample(self.buffer, batch_size)
+
+    def __len__(self):
+        return len(self.buffer)
+
+
+class PrioritizedExperienceReplay:
+    def __init__(
+        self,
+        buffer_length: int = 10000,
+        batch_size: int = 32,
+        alpha: float = 0.75,
+        beta0: float = 0.4,
+        replay_delay: int = 1000,
+    ):
+        self.buffer_length = buffer_length
+        self.batch_size = batch_size
+        self.alpha = alpha
+        self.beta0 = beta0
+        self.beta = beta0
+        self.priorities = deque([], maxlen=buffer_length)
+        self.total_steps = (buffer_length // batch_size) * replay_delay
+        self.buffer = deque([], maxlen=buffer_length)
+
+    def step(self):
+        self.beta = np.minimum(self.beta + (1 - self.beta0) / self.total_steps, 1)
+
+    def sample(self) -> Tuple[List, np.array, np.array]:
+        priorities_tmp = np.array(self.priorities)
+        probabilities = priorities_tmp ** self.alpha
+        probabilities /= probabilities.sum()
+
+        sample_indices = np.random.choice(len(self.buffer), size=self.batch_size, p=probabilities)
+        sample_transitions = [self.buffer[i] for i in sample_indices]
+
+        weights = (probabilities[sample_indices] * len(self.buffer)) ** (-self.beta)
+        weights = np.array(weights / weights.max(), dtype=np.float32)
+
+        return sample_transitions, sample_indices, weights
+
+    def push(self, *args):
+        if len(self.buffer) == 0:
+            self.priorities.append(1.0)
+        else:
+            self.priorities.append(np.mean(np.array(self.priorities)))
+            priorities_tmp = np.array(self.priorities)
+            priorities_tmp /= priorities_tmp.sum()
+            self.priorities = deque(priorities_tmp, maxlen=self.buffer_length)
+
+        self.buffer.append(utils.Transition(*args))
+
+    def update(self, indices: np.array, priorities: np.array):
+        priorities_tmp = np.array(self.priorities)
+        priorities_tmp[indices] = priorities
+        priorities_tmp /= priorities_tmp.sum()
+        self.priorities = deque(priorities_tmp, maxlen=self.buffer_length)
 
     def __len__(self):
         return len(self.buffer)
