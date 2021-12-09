1e690b700affe2e1216bd2305d7db69a6324aa67
Julius Rückin <julius.rueckin@gmail.com>
2021-12-09 11:45:41 +0100

    make Q-network lerger and its hidden dimension computation more flexible

--- networks/q_networks.py
+++ networks/q_networks.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 from torch.nn import functional as F
 
@@ -6,22 +7,27 @@ class DQN(nn.Module):
     def __init__(self, width: int = 84, height: int = 84, num_actions: int = 100):
         super(DQN, self).__init__()
 
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(8, 8), stride=4)
+        self.bn1 = nn.BatchNorm2d(32, track_running_stats=False)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=(4, 4), stride=2)
+        self.bn2 = nn.BatchNorm2d(64, track_running_stats=False)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=(3, 3), stride=1)
+        self.bn3 = nn.BatchNorm2d(64, track_running_stats=False)
 
-        def conv2d_size_out(size, kernel_size=5, stride=2):
-            return (size - (kernel_size - 1) - 1) // stride + 1
+        self.fc = nn.Linear(self.hidden_dimensions(width, height), 512)
+        self.head = nn.Linear(512, num_actions)
 
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(width)))
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(height)))
-        self.head = nn.Linear(convw * convh * 32, num_actions)
+    def hidden_dimensions(self, width: int = 84, height: int = 84) -> int:
+        x = F.relu(self.bn1(self.conv1(torch.rand((1, 3, width, height)))))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+
+        return int(torch.prod(torch.tensor(x.size())))
 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
-        return self.head(x.view(x.size(0), -1))
+
+        x = self.fc(x.view(x.size(0), -1))
+        return self.head(x)
