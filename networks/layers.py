e82f7d82e100117928c6897e07f6e74f4ec22893
Julius Rückin <julius.rueckin@gmail.com>
2022-03-14 17:01:54 +0100

    remove batch-norm layers in CNN encoder

--- networks/layers.py
+++ networks/layers.py
@@ -12,23 +12,20 @@ class Encoder(nn.Module):
         super(Encoder, self).__init__()
 
         self.conv1 = nn.Conv2d(const.FRAMES_STACKED, 32, kernel_size=(4, 4), stride=4)
-        self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=(4, 4), stride=2)
-        self.bn2 = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=(3, 3), stride=1)
-        self.bn3 = nn.BatchNorm2d(64)
 
     def hidden_dimensions(self, width: int = 84, height: int = 84) -> int:
-        x = F.relu(self.bn1(self.conv1(torch.rand((1, const.FRAMES_STACKED, width, height)))))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.conv1(torch.rand((1, const.FRAMES_STACKED, width, height))))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
 
         return int(torch.prod(torch.tensor(x.size())))
 
     def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
 
         return x
 
