compute value targets by cumulative returns, fix advantage calculation

networks/ppo_networks.py
@@ -34,7 +34,7 @@ class PPONet(nn.Module):
         x_policy = self.policy_head(x_policy)
         policy = F.softmax(x_policy, dim=1)
 
-        value = F.softplus(self.value_head(x_value))
+        value = self.value_head(x_value)
 
         return Categorical(policy), value
 
@@ -81,6 +81,6 @@ class ValueNet(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.encoder(x)
         x = F.relu(self.fc_value(x.view(x.size(0), -1)))
-        x = F.softplus(self.value_head(x))
+        x = self.value_head(x)
 
         return x

ppo/batch_memories.py
@@ -26,26 +26,51 @@ class BatchMemory:
         done: torch.Tensor,
         value: torch.Tensor,
     ):
-        self.transitions.append(TransitionPPO(state, action, policy, reward, done, value, torch.tensor([0])))
+        self.transitions.append(TransitionPPO(state, action, policy, reward, done, value, None, None))
 
-    def compute_advantages(self):
-        for t in range(len(self) - 1):
+    def compute_advantages(self, last_value: torch.Tensor, last_done: torch.Tensor):
+        last_done_idx = 0
+        for t in range(len(self)):
             advantage_t = 0
-            for i in range(t, len(self) - 1):
-                discount_factor = (const.GAMMA * const.LAMBDA) ** i
-                advantage_t += discount_factor * (
-                    self.transitions[i].reward
-                    + const.GAMMA * self.transitions[i + 1].value * (1 - self.transitions[i].done)
-                    - self.transitions[i].value
-                )
-
+            for i in range(t, len(self)):
+                discount_factor = (const.GAMMA * const.LAMBDA) ** (i - last_done_idx)
                 if self.transitions[i].done:
+                    last_done_idx = i
+                    advantage_t += discount_factor * (self.transitions[i].reward - self.transitions[i].value)
                     break
+
+                if i == const.HORIZON - 1:
+                    advantage_t += discount_factor * (
+                        self.transitions[i].reward + last_value * (1 - last_done) - self.transitions[i].value
+                    )
+                    break
+
+                advantage_t += discount_factor * (
+                    self.transitions[i].reward + const.GAMMA * self.transitions[i + 1].value - self.transitions[i].value
+                )
 
             self.transitions[t] = self.transitions[t]._replace(advantage=torch.tensor([advantage_t]))
 
-    def get(self) -> List:
-        self.compute_advantages()
+    def compute_returns(self, last_value: torch.Tensor, last_done: torch.Tensor):
+        last_done_idx = 0
+        for t in range(len(self)):
+            return_t = 0
+            for i in range(t, len(self)):
+                discount_factor = const.GAMMA ** (i - last_done_idx)
+                return_t += discount_factor * self.transitions[i].reward
+                if self.transitions[i].done:
+                    last_done_idx = i
+                    break
+
+                if i == const.HORIZON - 1:
+                    return_t += self.transitions[i].reward + discount_factor * last_value * (1 - last_done)
+
+            self.transitions[t] = self.transitions[t]._replace(return_t=torch.tensor([return_t]))
+
+    def get(self, last_value: torch.Tensor, last_done: torch.Tensor) -> List:
+        self.compute_returns(last_value, last_done)
+        self.compute_advantages(last_value, last_done)
+        self.normalize_values()
         batch_start_indices = np.arange(0, len(self), self.batch_size)
         transition_indices = np.arange(0, len(self), dtype=np.int32)
         np.random.shuffle(transition_indices)
@@ -56,6 +81,17 @@ class BatchMemory:
             batches.append([self.transitions[batch_index] for batch_index in batch_indices])
 
         return batches
+
+    def normalize_values(self):
+        adv_std = torch.cat([t.advantage for t in self.transitions]).squeeze().std().clamp(min=1e-8)
+        for i in range(len(self.transitions)):
+            norm_advantage = self.transitions[i].advantage / adv_std
+            self.transitions[i] = self.transitions[i]._replace(advantage=torch.tensor([norm_advantage]))
+
+        return_std = torch.cat([t.return_t for t in self.transitions]).squeeze().std().clamp(min=1e-8)
+        for i in range(len(self.transitions)):
+            norm_return = self.transitions[i].return_t / return_std
+            self.transitions[i] = self.transitions[i]._replace(return_t=torch.tensor([norm_return]))
 
     def __len__(self):
         return len(self.transitions)

ppo/ppo_wrappers.py
@@ -79,31 +79,43 @@ class PPOWrapper:
         self, transitions: List[TransitionPPO]
     ) -> Tuple[torch.Tensor, torch.Tensor, Categorical, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         batch = TransitionPPO(*zip(*transitions))
-        state_batch, action_batch, policy_batch, reward_batch, done_batch, value_batch, advantage_batch = (
+        state_batch, action_batch, policy_batch, reward_batch, value_batch, advantage_batch, return_batch = (
             torch.cat(batch.state),
             torch.cat(batch.action),
             torch.cat([dist.probs for dist in batch.policy]),
             torch.cat(batch.reward),
-            torch.cat(batch.done),
             torch.cat(batch.value),
             torch.cat(batch.advantage),
+            torch.cat(batch.return_t),
         )
 
-        state_batch, action_batch, policy_batch, reward_batch, done_batch, value_batch, advantage_batch = (
+        state_batch, action_batch, policy_batch, reward_batch, value_batch, advantage_batch, return_batch = (
             state_batch.to(self.device),
             action_batch.to(self.device),
             Categorical(policy_batch.to(self.device)),
             reward_batch.to(self.device),
-            done_batch.to(self.device),
             value_batch.to(self.device),
             advantage_batch.to(self.device),
+            return_batch.to(self.device),
         )
 
-        return state_batch, action_batch, policy_batch, reward_batch, done_batch, value_batch, advantage_batch
+        return state_batch, action_batch, policy_batch, reward_batch, value_batch, advantage_batch, return_batch
+
+    def compute_last_step_info(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        last_done = torch.tensor([int(self.batch_memory.transitions[-1].done)])
+        last_value = torch.tensor([0])
+
+        if not last_value:
+            last_state = self.batch_memory.transitions[-1].state
+            last_value = self.value_net_old(last_state)
+
+        return last_value.squeeze(), last_done.squeeze()
 
     def optimize_model(self, steps_done: int):
         clip_epsilon = schedule_clip_epsilon(const.CLIP_EPSILON, steps_done, const.NUM_EPISODES)
-        batches = self.batch_memory.get()
+        last_value, last_done = self.compute_last_step_info()
+        batches = self.batch_memory.get(last_value, last_done)
+
         for _ in range(const.NUM_EPOCHS):
             for batch in batches:
                 (
@@ -111,20 +123,20 @@ class PPOWrapper:
                     action_batch,
                     old_policy_batch,
                     reward_batch,
-                    done_batch,
                     old_value_batch,
                     advantage_batch,
+                    value_target_batch,
                 ) = self.prepare_batch_data(batch)
 
                 policy_batch = self.policy_net(state_batch)
                 value_batch = self.value_net(state_batch)
 
-                old_value_batch, value_batch, advantage_batch = (
+                old_value_batch, value_batch, advantage_batch, value_target_batch = (
                     old_value_batch.squeeze(),
                     value_batch.squeeze(),
                     advantage_batch.squeeze(),
+                    value_target_batch.squeeze(),
                 )
-                value_target_batch = advantage_batch + old_value_batch
 
                 policy_loss = self.get_policy_loss(
                     action_batch, policy_batch, old_policy_batch, advantage_batch, clip_epsilon

ppo/train.py
@@ -56,7 +56,6 @@ for i in range(const.NUM_EPISODES):
         _, reward, done, _ = env.step(action.item())
         episode_return += reward
         reward = torch.tensor([reward], device=device)
-        done_tensor = torch.tensor([int(done)], device=device)
         steps_done += 1
 
         next_observation = utils.get_screen(env)
@@ -66,7 +65,7 @@ for i in range(const.NUM_EPISODES):
             next_state.append(next_observation)
             next_state_tensor = torch.stack(tuple(next_state), dim=1)
 
-        ppo_wrapper.batch_memory.add(state_tensor, action, policy, reward, done_tensor, value)
+        ppo_wrapper.batch_memory.add(state_tensor, action, policy, reward, done, value)
         if len(ppo_wrapper.batch_memory) % const.HORIZON == 0 and len(ppo_wrapper.batch_memory) > 0:
             print(f"OPTIMIZE MODEL at STEP {steps_done}")
             ppo_wrapper.optimize_model(steps_done)