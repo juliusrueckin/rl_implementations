fix discount factor bug in PPO's value and advantage function calculation

ppo/batch_memories.py
@@ -29,13 +29,11 @@ class BatchMemory:
         self.transitions.append(TransitionPPO(state, action, policy, reward, done, value, None, None))
 
     def compute_advantages(self, last_value: torch.Tensor, last_done: torch.Tensor):
-        last_done_idx = 0
         for t in range(len(self)):
             advantage_t = 0
             for i in range(t, len(self)):
-                discount_factor = (const.GAMMA * const.LAMBDA) ** (i - last_done_idx)
+                discount_factor = (const.GAMMA * const.LAMBDA) ** (i - t)
                 if self.transitions[i].done:
-                    last_done_idx = i
                     advantage_t += discount_factor * (self.transitions[i].reward - self.transitions[i].value)
                     break
 
@@ -52,14 +50,12 @@ class BatchMemory:
             self.transitions[t] = self.transitions[t]._replace(advantage=torch.tensor([advantage_t]))
 
     def compute_returns(self, last_value: torch.Tensor, last_done: torch.Tensor):
-        last_done_idx = 0
         for t in range(len(self)):
             return_t = 0
             for i in range(t, len(self)):
-                discount_factor = const.GAMMA ** (i - last_done_idx)
+                discount_factor = const.GAMMA ** (i - t)
                 return_t += discount_factor * self.transitions[i].reward
                 if self.transitions[i].done:
-                    last_done_idx = i
                     break
 
                 if i == const.HORIZON - 1:

ppo/ppo_wrappers.py
@@ -11,7 +11,7 @@ from torch.utils.tensorboard import SummaryWriter
 import ppo_constants as const
 from networks.ppo_networks import PolicyNet, ValueNet
 from ppo.batch_memories import BatchMemory
-from utils.utils import explained_variance, TransitionPPO, schedule_clip_epsilon
+from utils.utils import explained_variance, TransitionPPO, schedule_clip_epsilon, clip_gradients
 
 
 class PPOWrapper:
@@ -151,20 +151,14 @@ class PPOWrapper:
                 )
                 self.policy_optimizer.zero_grad()
                 policy_loss.backward()
-
-                for param in self.policy_net.parameters():
-                    param.grad.data.clamp_(-const.CLIP_GRAD, const.CLIP_GRAD)
-
+                clip_gradients(self.policy_net, const.CLIP_GRAD)
                 self.policy_optimizer.step()
                 self.policy_scheduler.step()
 
                 value_loss = self.get_value_loss(value_batch, value_target_batch)
                 self.value_optimizer.zero_grad()
                 value_loss.backward()
-
-                for param in self.policy_net.parameters():
-                    param.grad.data.clamp_(-const.CLIP_GRAD, const.CLIP_GRAD)
-
+                clip_gradients(self.value_net, const.CLIP_GRAD)
                 self.value_optimizer.step()
                 self.value_scheduler.step()
 