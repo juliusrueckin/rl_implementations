fix bug in target Q-value calculation of SAC

networks/sac_networks.py
@@ -11,10 +11,11 @@ from networks.layers import MLPEncoder
 
 
 class PolicyNet(nn.Module):
-    def __init__(self, state_dim: int, action_dim: int, num_fc_hidden_units: int = 256):
+    def __init__(self, state_dim: int, action_dim: int, action_limits: torch.Tensor, num_fc_hidden_units: int = 256):
         super(PolicyNet, self).__init__()
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.action_limits = action_limits
 
         self.encoder = MLPEncoder(state_dim, num_fc_hidden_units)
         self.fc_mean = nn.Linear(num_fc_hidden_units, num_fc_hidden_units)
@@ -40,8 +41,8 @@ class PolicyNet(nn.Module):
     def evaluate(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, Independent]:
         policy = self.forward(x)
         u = policy.rsample()
-        action = torch.tanh(u)
         log_prob = policy.log_prob(u) - torch.sum(2 * (np.log(2) - u - F.softplus(-2 * u)), dim=1)
+        action = self.action_limits * torch.tanh(u)
         return action, log_prob, policy
 
 

sac/sac_wrapper.py
@@ -6,6 +6,7 @@ import numpy as np
 import torch
 from torch import nn
 from torch.utils.tensorboard import SummaryWriter
+from torch.distributions.independent import Independent
 
 import sac_constants as const
 from networks.sac_networks import PolicyNet, QNet
@@ -18,11 +19,13 @@ from utils.utils import (
     compute_grad_norm,
     log_network_params,
     normalize_values,
+    ValueStats,
 )
 
 
 class SACWrapper:
-    def __init__(self, state_dim: int, num_actions: int, writer: SummaryWriter = None):
+    def __init__(self, state_dim: int, num_actions: int, action_limits: torch.tensor, writer: SummaryWriter = None):
+        self.value_stats = ValueStats()
         self.num_actions = num_actions
         self.writer = writer
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -32,9 +35,9 @@ class SACWrapper:
             const.N_STEP_RETURNS,
         )
 
-        self.policy_net = PolicyNet(state_dim, num_actions, num_fc_hidden_units=const.NUM_FC_HIDDEN_UNITS).to(
-            self.device
-        )
+        self.policy_net = PolicyNet(
+            state_dim, num_actions, action_limits, num_fc_hidden_units=const.NUM_FC_HIDDEN_UNITS
+        ).to(self.device)
 
         self.q_net1 = QNet(state_dim, num_actions, num_fc_hidden_units=const.NUM_FC_HIDDEN_UNITS).to(self.device)
         self.target_q_net1 = QNet(state_dim, num_actions, num_fc_hidden_units=const.NUM_FC_HIDDEN_UNITS).to(self.device)
@@ -74,7 +77,7 @@ class SACWrapper:
 
     @staticmethod
     def get_policy_loss(log_probs: torch.tensor, q_values: torch.tensor) -> torch.tensor:
-        return -torch.mean(q_values.detach() - const.ENTROPY_COEFF * log_probs)
+        return (const.ENTROPY_COEFF * log_probs - q_values).mean()
 
     @staticmethod
     def get_q_value_loss(estimated_q_values: torch.tensor, q_value_targets: torch.tensor) -> torch.Tensor:
@@ -127,11 +130,11 @@ class SACWrapper:
                     self.target_q_net1(non_final_next_states, new_next_action_batch),
                     self.target_q_net2(non_final_next_states, new_next_action_batch),
                 )
-                target_q_values[non_final_mask] += (
-                    const.GAMMA ** const.N_STEP_RETURNS
-                ) * target_next_q_values.squeeze() - const.ENTROPY_COEFF * new_next_log_prob_batch.squeeze()
+                target_q_values[non_final_mask] += (const.GAMMA ** const.N_STEP_RETURNS) * (
+                    target_next_q_values.squeeze() - const.ENTROPY_COEFF * new_next_log_prob_batch.squeeze()
+                )
                 if const.NORMALIZE_VALUES:
-                    target_q_values = normalize_values(target_q_values, shift_mean=False)
+                    target_q_values = self.value_stats.normalize(target_q_values, shift_mean=False)
 
             estimated_q_values1 = self.q_net1(state_batch, action_batch.unsqueeze(1)).squeeze()
             q_value_loss1 = self.get_q_value_loss(estimated_q_values1, target_q_values)
@@ -147,16 +150,25 @@ class SACWrapper:
             clip_gradients(self.q_net2)
             self.q_net2_optimizer.step()
 
+            for param in self.q_net1.parameters():
+                param.requires_grad = False
+            for param in self.q_net2.parameters():
+                param.requires_grad = False
+
             new_action_batch, log_prob_batch, new_policy = self.policy_net.evaluate(state_batch)
-            with torch.no_grad():
-                estimated_new_q_values = torch.min(
-                    self.q_net1(state_batch, new_action_batch), self.q_net2(state_batch, new_action_batch)
-                )
+            estimated_new_q_values = torch.min(
+                self.q_net1(state_batch, new_action_batch), self.q_net2(state_batch, new_action_batch)
+            )
             policy_loss = self.get_policy_loss(log_prob_batch, estimated_new_q_values)
             self.policy_optimizer.zero_grad()
             policy_loss.backward()
             clip_gradients(self.policy_net)
             self.policy_optimizer.step()
+
+            for param in self.q_net1.parameters():
+                param.requires_grad = True
+            for param in self.q_net2.parameters():
+                param.requires_grad = True
 
         if self.writer:
             self.track_tensorboard_metrics(
@@ -167,7 +179,7 @@ class SACWrapper:
                 estimated_q_values1,
                 target_q_values,
                 explained_variance(estimated_q_values1, target_q_values),
-                new_policy.entropy().mean(),
+                new_policy,
             )
 
     def track_tensorboard_metrics(
@@ -179,14 +191,16 @@ class SACWrapper:
         estimated_q_values: torch.tensor,
         target_q_values: torch.tensor,
         explained_var: float,
-        entropy: float,
+        policy: Independent,
     ):
-        self.writer.add_scalar(f"Training/Policy-Entropy", entropy, steps_done)
+        self.writer.add_scalar(f"Training/Policy-Entropy", policy.entropy().mean(), steps_done)
+        self.writer.add_scalar(f"Training/Policy-Mean", policy.base_dist.loc.mean(), steps_done)
+        self.writer.add_scalar(f"Training/Policy-Std", policy.base_dist.scale.mean(), steps_done)
         self.writer.add_scalar("Training/Policy-Loss", policy_loss, steps_done)
+
         self.writer.add_scalar("Training/Q-Value1-Loss", q_value_loss1, steps_done)
         self.writer.add_scalar("Training/Q-Value2-Loss", q_value_loss2, steps_done)
-
-        self.writer.add_scalar("Training/Q-Value-Estimation", estimated_q_values.mean(), steps_done)
+        self.writer.add_scalar("Training/Q-Value1-Estimation", estimated_q_values.mean(), steps_done)
         self.writer.add_scalar("Training/Q-Value-Target", target_q_values.mean(), steps_done)
         self.writer.add_scalar("Training/Q-Value-Explained-Variance", explained_var, steps_done)
 

sac/train.py
@@ -16,12 +16,14 @@ env.reset()
 
 state_dim = env.observation_space.shape[0]
 action_dim = env.action_space.shape[0]
+action_limits = torch.from_numpy(env.action_space.high).to(device=device).unsqueeze(0)
 steps_done = 0
 
-sac_wrapper = SACWrapper(state_dim, action_dim, writer=writer)
+sac_wrapper = SACWrapper(state_dim, action_dim, action_limits, writer=writer)
 
 for i in range(const.NUM_EPISODES):
     physical_state = env.reset()
+    env.render(mode="rgb_array")
     physical_state_tensor = torch.tensor(physical_state, device=device).unsqueeze(0)
 
     episode_return = 0
@@ -35,7 +37,8 @@ for i in range(const.NUM_EPISODES):
             continue
 
         if t % const.ACTION_REPETITIONS != 0:
-            _, reward, done, _ = env.step(u.cpu().numpy())
+            _, reward, done, _ = env.step(action.cpu().numpy())
+            env.render(mode="rgb_array")
             episode_return += reward
             if done:
                 sac_wrapper.episode_terminated(episode_return, steps_done)
@@ -44,14 +47,12 @@ for i in range(const.NUM_EPISODES):
             continue
 
         if steps_done < const.MIN_START_STEPS:
-            u = env.action_space.sample()
-            u = torch.from_numpy(u).to(device)
-            action = torch.tanh(u).to(device)
+            action = torch.from_numpy(env.action_space.sample()).to(device)
         else:
             action = sac_wrapper.policy_net.get_action(physical_state_tensor).squeeze(1).detach()
-            u = torch.from_numpy(env.action_space.high).to(device) * action
 
-        next_physical_state, reward, done, _ = env.step(u.cpu().numpy())
+        next_physical_state, reward, done, _ = env.step(action.cpu().numpy())
+        env.render(mode="rgb_array")
         next_physical_state_tensor = torch.tensor(next_physical_state, device=device).unsqueeze(0)
         episode_return += reward
         reward = torch.tensor([reward], device=device)